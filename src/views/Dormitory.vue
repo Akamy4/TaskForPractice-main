96bc9f1e8146ca2138a1b7ff8a6252b9d78dc88d
Akame <abdrakhmanaznabakiev@gmail.com>
2022-02-11 18:44:45 +0300

    new

---
+++ src/views/Dormitory.vue
@@ -0,0 +1,356 @@
+<template>
+    <div class="Main-block">
+        <div class="First-block container">
+            <div class="header-text">
+                <p>Общежитие</p>
+            </div>
+            <div class="socialNetworks">
+                <div class="Instagram">
+                    <a href="https://www.instagram.com/aues_university/" target="_blank"><img  src="../assets/pictures/instagramPic.png"></a>
+                </div>
+                <div class="TikTok">
+                    <a href="https://www.tiktok.com/@aues_uni?_d=secCgwIARCbDRjEFSADKAESPgo8hIdI1RIxDykmY%2B117i6Swo1QNokg09PMkhH5UI%2FQWqYQPG9PNNBWy8HyBgG9%2BR5P97jikRevfeMuKKeCGgA%3D&language=ru&sec_uid=MS4wLjABAAAAybvrGildxqKzbHDi4v5MKovEme7EAUFFT_6G4_Ss4I8Ry1ov5v5R66K-MMdMkHdl&share_app_name=musically&share_author_id=6844731653044028422&timestamp=1643715567&u_code=dacc1g3g483209&user_id=6783552851463308293&utm_campaign=client_share&utm_medium=android&utm_source=more&source=h5_m&_r=1" target="_blank"><img  src="../assets/pictures/tik-tok.png"></a>
+                </div>
+                <div class="Gmail">
+                    <a href="#" target="_blank"><img  src="../assets/pictures/Gmail.png"></a>
+                </div>
+                <div class="Youtube">
+                    <a href="https://www.youtube.com/channel/UCUnDGC1ddotzf1fpn-hyXDA" target="_blank"><img  src="../assets/pictures/youtube.png"></a>
+                </div>
+            </div>
+        </div>
+        <div class="Second-block">
+            <div class="container-Corruption">
+                <div class="Background">
+                    <div class="Top container-Dormitory">
+                        <div class="TopInfo">
+                            <span>Кто может проживать в общежитиях АУЭС?</span>
+                            <span>У кого есть преимущество при заселении? </span>
+                            <span>Какие и у каких категорий имеются льготы?</span>
+                            <span>Какие документы необходимо представить для заселения в общежитие? </span>
+                            <span>Какая процедура предусмотрена при предоставлении мест в общежитии? </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="DormitoryFlex">
+                    <div class="DormitoryTop">
+                                <p>В общежитиях университета имеют право проживать студенты, обучающиеся по государственному образовательному заказу или на полноплатной основе и приехавшие из отдаленных областей Республики Казахстан, а также граждане иностранных государств.</p>
+                            </div>
+                            <div class="DormitoryTop">
+                                <p>Местами в общежитии в первую очередь обеспечиваются следующие категории обучающихся: </p>
+                                <ul>
+                                    <li>обучающиеся, оставшиеся на момент достижения совершеннолетия без попечения родителей;</li>
+                                    <li>обучающиеся с особыми возможностями здоровья из числа инвалидов I и II группы, детей-инвалидов или при наличии родителей-инвалидов I и II группы (обоих или одного);</li>
+                                    <li>обучающиеся из числа сельской молодежи, поступившие на специальности, определяющие социально-экономическое развитие села;</li>
+                                    <li>обучающиеся по государственному образовательному заказу или на полноплатной основе и приехавшие из отдаленных областей Республики;</li>
+                                    <li>обучающиеся из многодетных семей, имеющих 3-х и более детей в возрасте до 18 лет, помимо обучающегося, которому должно быть не более 21 года в период обучения.</li>
+                                </ul>
+                            </div>
+                            <div class="DormitoryTop">
+                                <p>С обучающихся из числа детей-сирот, детей с особыми возможностями здоровья и детей, оставшихся без попечения родителей и находящихся под опекой (попечительством), плата за проживание не взимается.</p>
+                                <br>
+                                <span>Примечание: обучающиеся, имеющие право на льготы (см. 2 и 3 вопросы) обязаны дополнительно представить сканы следующих документов:</span>
+                                <ul>
+                                    <li>свидетельство о смерти родителей; </li>
+                                    <li>свидетельство об инвалидности родителей или одного из членов семьи;</li>
+                                    <li>свое свидетельство о рождении и свидетельства о рождении своих младших братьев и сестер в возрасте до 18 лет;</li>
+                                    <li>другие документы, подтверждающие предоставление льгот.</li>
+                                </ul>
+                            </div>
+                            <div class="DormitoryTop">
+                                <p>Необходимо представить следующие документы: </p>
+                                <ul>
+                                    <li>удостоверение личности (копия - 3 штуки);</li>
+                                    <li>студентам старше 18 лет - паспорт вакцинации (в случае противопоказаний по вакцинации необходимо предоставить справку ВКК, формы № 026 или № 027);</li>
+                                    <li>фотографии 3/4 – 3 штуки; </li>
+                                    <li>флюорографию;</li>
+                                    <li>ПЦР-тест (не менее чем за 72 часа) на COVID -19 (студентам не достигшим 18 лет и для лиц, имеющих противопоказания по вакцинации).</li>
+                                </ul>
+                            </div>
+                            <div class="DormitoryTop">
+                                <p>Основанием для предоставления мест в общежитии является заявление (установленного образца на имя ректора).  </p>
+                                <br>
+                                <span>Заявление подается:  </span>
+                                <ul>
+                                    <li>на сайт Университета (все обязательные документы прикладываются в отсканированном виде);</li>
+                                    <li>уведомление об отказе или об удовлетворении поданного обучающимися заявления обеспечивается в срок не позднее 3-х рабочих дней;</li>
+                                    <li>с обучающимися, чьи заявления были одобрены комиссией по распределению мест в общежитиях АУЭС, подписывается Договор аренды;</li>
+                                    <li>после подписания Договора и с внесением первой оплаты (не менее, чем за 1 месяц проживания) обучающийся также оплачивает страховой депозит, 20 000 (двадцать тысяч) тенге, который возвращается обучающемуся после окончания Договора, при условии соблюдения обучающимся всех правил проживания в общежитии и отсутствии нанесенного ущерба.</li>
+                                </ul>
+                            </div>
+                </div>
+            </div>
+            <div class="Bpttom">
+                <div class="container-Dormitory fleX">
+                    <div class="BpttomFooter">
+                        <div class="Background">
+                            <div class="TopInfoBtn">
+                                <span>Фотогалерея</span>
+                            </div>
+                        </div>
+                                <div class="PhotoChoice Indent">
+                                        <img src="" alt="Здесь должно было быть фото">
+                                </div>
+                    </div>
+                    <div class="BpttomFooter">
+                        <div class="Background mrg">
+                            <div class="TopInfoBtn">
+                                <span>Прейскурант</span>
+                            </div>
+                        </div>
+                        <div class="Indent">
+                            <table border="1" border-color="black" class="Table">
+                                <div class="BackgroundIndent">
+                                    <div class="TextIndent">
+                                        <span>Месячная оплата при длительном проживании</span>
+                                    </div>
+                                </div>
+                                <tr> 
+                                    <th rowspan="2">Категория жильцов</th>
+                                    <th colspan="2">Стоимость проживания в общежитии № 1,тг.</th>
+                                    <th colspan="3">Стоимость проживания в общежитии № 2,тг.</th>
+                                    <th colspan="2">Стоимость проживания в общежитии № 3,тг.</th>
+                                </tr>
+                                <tr>
+                                    <td>2-мест.</td>
+                                    <td>4-мест.</td>
+                                    <td>2-мест.</td>
+                                    <td>3-мест.</td>
+                                    <td>4-мест.</td>
+                                    <td>2-мест.</td>
+                                    <td>4-мест.</td>
+                                </tr>
+                                <tr>
+                                    <td>Колледж</td>
+                                    <td>14 000</td>
+                                    <td>9 000</td>
+                                    <td>17 500</td>
+                                    <td>13 500</td>
+                                    <td>11 500</td>
+                                    <td>14 000</td>
+                                    <td>8 000</td>
+                                </tr>
+                                <tr>
+                                    <td>Бакалавриат,магистратура,докторантура</td>
+                                    <td>18 000</td>
+                                    <td>12 000</td>
+                                    <td>26 000</td>
+                                    <td>24 000</td>
+                                    <td>22 000</td>
+                                    <td>21 000</td>
+                                    <td>15 000</td>
+                                </tr>
+                                <tr>
+                                    <td>Работники АУЭС</td>
+                                    <td>30 000</td>
+                                    <td>18 000</td>
+                                    <td>33 500</td>
+                                    <td>27 500</td>
+                                    <td>23 500</td>
+                                    <td>30 000</td>
+                                    <td>20 000</td>
+                                </tr>
+                                <tr>
+                                    <td>Иные</td>
+                                    <td>35 000</td>
+                                    <td>20 000</td>
+                                    <td>38 500</td>
+                                    <td>33 500</td>
+                                    <td>25 500</td>
+                                    <td>35 000</td>
+                                    <td>22 000</td>
+                                </tr>
+                                
+                                <div class="BackgroundIndent">
+                                    <div class="TextIndent">
+                                        <span>Суточная оплата при кратковременном проживании (при наличии свободных мест)</span>
+                                    </div>
+                                </div>
+                            
+                            <tr>
+                                   <td>Абитуриенты</td> 
+                                   <td>3000</td>
+                                   <td>1500</td>
+                                   <td>3500</td>
+                                   <td>3000</td>
+                                   <td>2500</td>
+                                   <td>3000</td>
+                                   <td>2000</td>
+                                </tr>
+                                 <tr>
+                                   <td>Родители и родственники обучающихся</td> 
+                                   <td>4000</td>
+                                   <td>2000</td>
+                                   <td>4500</td>
+                                   <td>3500</td>
+                                   <td>3000</td>
+                                   <td>4000</td>
+                                   <td>2500</td>
+                                </tr>
+                                 <tr>
+                                   <td>Иные</td> 
+                                   <td>7000</td>
+                                   <td>3000</td>
+                                   <td>7500</td>
+                                   <td>6500</td>
+                                   <td>4500</td>
+                                   <td>7000</td>
+                                   <td>4000</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="BpttomFooter">
+                        <div class="Background ">
+                            <div class="TopInfoBtn">
+                                <span>Адреса общежития</span>
+                            </div>
+                        </div>
+                        <div class="flexFooter">
+                                <div class="IndentFoot">
+                                    <span class="BigTextDormitory">Общежитие №1</span>
+                                    <br>
+                                    <span>г.Алматы, ул. Политехническая, 1</span>
+                                    <br>
+                                    <span>тел.: +7 (727) 292 90 93</span>
+
+                                        <br>
+                                        <br>
+
+                                    <p class="BigTextDormitory">Общежитие №2</p>
+                                    <span>г.Алматы, ул. Зеина Шашкина, 14а</span>
+                                    <br>
+                                    <span>тел.: +7 (727) 292 90 93</span>
+
+                                        <br>
+                                        <br>
+
+                                    <p class="BigTextDormitory">Общежитие №3</p>
+                                    <span>г.Алматы, ул. Зеина Шашкина, 14</span>
+                                    <br>
+                                    <span>тел.: +7 (727) 264 16 84</span>
+                            </div>
+                            <div class="link">
+                                <span class="BigTextDormitory">По вопросам:</span>
+                                <br>
+                                <br>
+                                <p>Демчук Кирилл</p>
+                                <a href="#"><span>k.demchuk@aues.kz </span></a>
+                                <br>
+                                <br>
+                                <p>Каракесов Мансур</p>
+                                <a href="#"><span>m.karakesov@aues.kz</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+@import '/src/assets/index.css';
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500&family=Roboto:ital,wght@0,300;1,300&display=swap');
+
+*{margin:0; padding:0;}
+.header-text {
+    font-family: 'Roboto Slab', serif;
+    font-size: 170%;
+    font-weight:500;
+    color:blue;
+}
+.TopInfo{
+    color:white;
+    display:flex;
+    justify-content:space-between;
+    font-family: 'Roboto Slab', serif;
+    font-size: 105%;
+    font-weight:200;
+    text-align: center;
+}
+.TopInfo > span{
+    padding-top:0.5%;
+    padding-bottom:0.5%;
+}
+.TopInfoBtn{
+    color:white;
+    font-family: 'Roboto Slab', serif;
+    font-size: 120%;
+    font-weight:500;
+    text-align: center;
+}
+.TopInfo > span{
+    padding-top:1%;
+    padding-bottom:1%;
+}
+.Background{
+    background-color:blue;
+    text-align: center;
+}
+.DormitoryTop{
+    max-width: 18%;
+    color:blue;
+    font-family: 'Roboto Slab', serif;
+    margin-top:2%;
+}
+.DormitoryFlex{
+    display:flex;
+}
+.BpttomFooter{
+    width:33%;
+}
+.fleX{
+    display:flex;
+    margin-top: 2%;
+}
+.mrg{
+    margin-right: 0.5%;
+    margin-left: 0.5%;
+}
+.Indent{
+    margin-top:2%;
+}
+.FR{
+    border:2px;
+    border-color:black;
+}
+.Table{
+    color:blue;
+    font-family: 'Roboto Slab', serif;
+}
+.TextIndent{
+    color:white;
+}
+table > div{
+    background-color:blue;
+    width: 208%;
+}
+table > .BackgroundIndent{
+    text-align:center;
+}
+.IndentFoot{
+    margin-top:2%;
+    color:blue;
+}
+.BigTextDormitory{
+    font-family: 'Roboto Slab', serif;
+    font-weight: 700;
+}
+.link{
+    color:blue;
+    margin-top: 2%;
+}
+.link > a{
+    text-decoration:none;
+    color:blue;
+}
+.flexFooter{
+    display:flex;
+    justify-content:space-between;
+}
+</style>
+
+
+
+
